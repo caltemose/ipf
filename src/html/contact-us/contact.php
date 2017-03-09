8ed80380843a070a5f2763d13c5052275cb07d95
Chad Altemose <chad@chadzilla.com>
2017-03-09 03:16:06 -0500

    content updates and integration of mailgun into contact.php

--- src/html/contact-us/contact.php
+++ src/html/contact-us/contact.php
@@ -1,4 +1,18 @@
 <?php
+// In order to make this form work, some dependencies are required.
+// See Mailgun docs: https://documentation.mailgun.com/libraries.html#php
+// Install Composer
+# curl -sS https://getcomposer.org/installer | php
+// Add Mailgun and Guzzle6 as a dependency
+# php composer.phar require mailgun/mailgun-php php-http/guzzle6-adapter php-http/message
+
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+# Instantiate the client.
+$mgClient = new Mailgun('key-531fa3a409cd408c0c594febe61dd547');
+$domain = "mg.inmanparkfestival.org";
+
 // get POST data
 $visitorName = Trim(stripslashes($_POST['name']));
 $visitorEmail = Trim(stripslashes($_POST['email']));
@@ -73,41 +87,58 @@ if (!$valid) {
 $recipientList = '';
 switch($recipient) {
     case 'chair':
-        $recipientList = 'chair';
+        $recipientList = 'festival@inmanparkfestival.org';
         break;
     case 'dance':
-        $recipientList = 'dance';
+        $recipientList = 'dance@inmanparkfestival.org';
         break;
     case 'market':
-        $recipientList = 'market';
+        $recipientList = 'vendors@inmanparkfestival.org';
         break;
     case 'parade':
-        $recipientList = 'parade';
+        $recipientList = 'parade@inmanparkfestival.org';
         break;
     case 'food':
-        $recipientList = 'food';
+        $recipientList = 'food.vendors@inmanparkfestival.org';
         break;
     case 'music':
-        $recipientList = 'music';
+        $recipientList = 'entertainment@inmanparkfestival.org';
         break;
     case 'toh':
-        $recipientList = 'toh';
+        $recipientList = 'tickets@inmanparkfestival.org';
         break;
     case 'sponsors':
-        $recipientList = 'sponsors';
+        $recipientList = 'sponsors@inmanparkfestival.org';
         break;
     case 'volunteers':
-        $recipientList = 'volunteers';
+        $recipientList = 'volunteer@inmanparkfestival.org';
         break;
     case 'pr':
-        $recipientList = 'pr';
+        $recipientList = 'pr@inmanparkfestival.org';
         break;
     case 'marketing':
-        $recipientList = 'marketing';
+        $recipientList = 'advertising@inmanparkfestival.org';
+        break;
+    case 'support':
+        $recipientList = 'support@inmanparkfestival.org';
         break;
     default:
-        $recipientList = 'chair';
+        $recipientList = 'festival@inmanparkfestival.org';
 }
+
+$messageBody  = "To respond to this email, copy the email address shown below into a new message please.\n";
+$messageBody .= "Email sent from the website contact form from:\n";
+$messageBody .= $visitorName . ' - ' . $visitorEmail . "\n\n";
+$messageBody .= $message;
+
+# Make the call to the client.
+$result = $mgClient->sendMessage($domain, array(
+    'from'    => 'Site Admin <admin@inmanparkfestival.org>',
+    'to'      => $recipientList,
+    'subject' => 'Email from Festival Contact Form',
+    'text'    => $messageBody
+));
+$success["result"] = $result;
 $success["recipientList"] = $recipientList;
 
 $res = json_encode($success);
